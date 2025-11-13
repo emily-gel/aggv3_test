be1c027adfebc0e53f2db364f320ab6521dc41ca
Emily Perry <95911323+emily-gel@users.noreply.github.com>
2025-11-13 14:46:21 +0000

    Update vcf_to_result.py

--- bin/vcf_to_result.py
+++ bin/vcf_to_result.py
@@ -6,14 +6,12 @@ import pandas
 from pysam import VariantFile
 
 @click.command()
-@click.option( "--bed_intersect", required=True)
+@click.option( "--vcf", required=True)
 @click.option( "--sample_list", required=True)
 
-def shard_to_result (bed_intersect, sample_list):
+def vcf_to_result (vcf, sample_list):
 
-    intersect = pandas.read_table(bed_intersect, header = None)
-    intersect['s3_string'] = intersect[10] + intersect[13]
-    shard_vcf = VariantFile(intersect.loc[0, 's3_string']) 
+    shard_vcf = VariantFile(vcf) 
 
     filtered = shard_vcf.fetch(locus.split(":")[0], int(locus.split(":")[1].split("-")[0]), int(locus.split(":")[1].split("-")[1]))
     rows = pandas.DataFrame(columns=['chrom', 'pos', 'ref', 'alt', 'ID', 'genotype'])
@@ -29,4 +27,4 @@ def shard_to_result (bed_intersect, sample_list):
     rows.to_csv('results.csv', index=False)
 
 if __name__ == "__main__":
-    shard_to_result()
+    vcf_to_result()
